a3e836e0b351837653eadaaba35320d777ed0a36
jekeytimoy29 <mothj20@gmail.com>
2026-02-09 04:49:18 +0800

    Configure next.config.js for portfolio deployment
    Set output, basePath, and assetPrefix for deployment.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
+  output: 'export',
+  basePath: '/my-portfolio-v2-with-aws',
+  assetPrefix: '/my-portfolio-v2-with-aws',
   reactStrictMode: false,
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    unoptimized: true
   },
 };
