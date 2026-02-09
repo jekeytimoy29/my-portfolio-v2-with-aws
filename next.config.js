6294aaa5cfd28ae7e55df518872052e731446b89
jekeytimoy29 <mothj20@gmail.com>
2026-02-09 08:32:39 +0800

    Remove output and basePath settings from config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  output: 'export',
-  basePath: '/my-portfolio-v2-with-aws',
-  assetPrefix: '/my-portfolio-v2-with-aws/',
   reactStrictMode: false,
   images: {
-    unoptimized: true
+    domains: ['avatars.githubusercontent.com'],
   },
 };
