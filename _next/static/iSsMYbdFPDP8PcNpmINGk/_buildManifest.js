3f72ead783d8f8b072ddb44df5d1de3d9a33f719
Timothy Jake Flordelis <mothj20@gmail.com>
2026-02-09 08:01:05 +0800

    Updates

---
+++ _next/static/iSsMYbdFPDP8PcNpmINGk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/900-21b432f509ba1d44.js","static/chunks/pages/index-e97c9691bc2b5f1e.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
