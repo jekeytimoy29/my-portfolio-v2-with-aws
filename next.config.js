7281f629a4557f2ec82562afa312c501e4c8f610
jekeytimoy29 <mothj20@gmail.com>
2026-02-14 06:17:06 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
-
 module.exports = {
-  output: "export",
-  basePath: repo ? `/${repo}` : "",
-  assetPrefix: repo ? `/${repo}/` : "",
-  images: { unoptimized: true },
+  output: 'export',
+  basePath: '/my-portfolio-v2-with-aws',
+  assetPrefix: '/my-portfolio-v2-with-aws/',
+  reactStrictMode: false,
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+    unoptimized: true
+  },
 };
